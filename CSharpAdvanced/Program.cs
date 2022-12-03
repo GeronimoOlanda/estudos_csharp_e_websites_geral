1ed84d5aa569a3613c8be40a56d1985e66fc5b0e
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-03 12:02:50 -0300

    Trabalhando com listar

--- CSharpAdvanced/Program.cs
+++ CSharpAdvanced/Program.cs
@@ -1,2 +1,21 @@
-﻿string message = "Ola Mundo!";
-Console.WriteLine($"{message}");
+﻿IList<string> pessoas = new List<string>();
+
+
+var nome = "";
+while(nome != "Parou")
+{
+    Console.WriteLine("Digite o nome...");
+    nome = Console.ReadLine();
+    Console.WriteLine($"Seu nome é {nome}");
+
+
+    if (nome != null)
+    {
+        pessoas.Add(nome);
+    }
+   
+}
+foreach (var pe in pessoas)
+{
+    Console.WriteLine(pe);
+}
