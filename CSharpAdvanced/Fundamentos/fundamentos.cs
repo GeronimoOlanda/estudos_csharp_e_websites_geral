5aa0effd5b79d0507ede7a42d5a95a42c1550476
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-05 20:08:12 -0300

    fundamentos basicos

--- CSharpAdvanced/Fundamentos/fundamentos.cs
+++ CSharpAdvanced/Fundamentos/fundamentos.cs
@@ -8,7 +8,23 @@ namespace CSharpAdvanced.Fundamentos
 {
     public class fundamentos
     {
+        // exibe a media dos valores
+        public double CalculoMedia(double valor1, double valor2)
+        {
+            double media = ((valor1 + valor2) / 2);
 
+            return media;
+        }
 
+        //retorna a mensagem
+        public string Mensagem(string mensagem)
+        {
+            return mensagem;
+        }
+
+        public string MensagemErro(string erro)
+        {
+            return  erro;
+        }
     }
 }
