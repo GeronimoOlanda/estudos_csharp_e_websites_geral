100d30fb93d1d97f3972fed9b2c5f72f8437e529
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-05 20:03:10 -0300

    instanciação dos atributos, e criação dto e model.

---
+++ CSharpAdvanced/Models/DTOs/PessoaFisicaDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.Models.DTOs
+{
+    public class PessoaFisicaDTO
+    {
+        public string Id { get; set; }
+        public string CPF { get; set; }
+    }
+}
--- CSharpAdvanced/Models/PessoaModel.cs
+++ CSharpAdvanced/Models/PessoaModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpAdvanced.Models.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
