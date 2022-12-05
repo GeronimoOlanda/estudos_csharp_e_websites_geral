cf279d2b05ddf71dee9756a3335252bca712f5eb
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-05 20:00:16 -0300

    criação dos atributos.

--- CSharpAdvanced/Models/PessoaModel.cs
+++ CSharpAdvanced/Models/PessoaModel.cs
@@ -8,5 +8,10 @@ namespace CSharpAdvanced.Models
 {
     public class PessoaModel
     {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public DateTime DataAtual { get; set; }
+        public DateTime DataNascimento { get; set; }
     }
 }
