1c6d45f23daefc798250bb36253544c5ee0f21f3
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-08 09:53:20 -0300

    comment

--- CSharpAdvanced/Program.cs
+++ CSharpAdvanced/Program.cs
@@ -3,26 +3,26 @@
 IList<string> pessoas = new List<string>();
 
 
-var nome = "";
-while(nome != "Parou")
-{
-    Console.WriteLine("Digite o nome...");
-    nome = Console.ReadLine();
-    Console.WriteLine($"Seu nome é {nome}");
+//var nome = "";
+//while(nome != "Parou")
+//{
+//    Console.WriteLine("Digite o nome...");
+//    nome = Console.ReadLine();
+//    Console.WriteLine($"Seu nome é {nome}");
 
 
-    if (nome != null)
-    {
-        pessoas.Add(nome);
-    }
+//    if (nome != null)
+//    {
+//        pessoas.Add(nome);
+//    }
    
-}
-foreach (var pe in pessoas)
-{
-    Console.WriteLine(pe);
-}
+//}
+//foreach (var pe in pessoas)
+//{
+//    Console.WriteLine(pe);
+//}
 
 
-Fundamentos fundamentos = new Fundamentos();
+//Fundamentos fundamentos = new Fundamentos();
 
-Console.WriteLine(fundamentos.Mensagem("Louvado seja o nome do senhor Jesus Cristo!"));
+//Console.WriteLine(fundamentos.Mensagem("Louvado seja o nome do senhor Jesus Cristo!"));
