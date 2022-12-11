52817ef67bdd1f2453dadfb4b28d34bacb5254f7
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-11 00:06:23 -0300

    criação da nossa model categoria.

---
+++ CSharpAdvanced/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.Models
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        public string Titullo  { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataPublicacao { get; set; }
+        public DateTime DataCriacaoBase { get
+            {
+                return DateTime.Now;
+            }
+        }
+    }
+}
