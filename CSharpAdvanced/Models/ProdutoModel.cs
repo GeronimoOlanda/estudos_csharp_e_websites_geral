754ae1c0e25d067941a02d5f3957d70cb1e4f04c
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-10 22:01:01 -0300

    criação model Produto

---
+++ CSharpAdvanced/Models/ProdutoModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced.Models
+{
+    public class ProdutoModel
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public ProdutoModel(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        /// <summary>
+        /// sobrecarga de metodo
+        /// </summary>
+        /// <returns></returns>
+       public ProdutoModel(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 0;
+        }
+        public double ValotTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = quantidade;
+        }
+    }
+}
