96cead76b28f64a2f53edb66d88fa3d4a03e2a90
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-12 13:26:53 -0300

    chamando a nossa model contatos na home.

---
+++ GeroOlanda.API/Models/Home/HomeModel.cs
@@ -0,0 +1,16 @@
+﻿using GeroOlanda.API.Models.Contatos;
+
+namespace GeroOlanda.API.Models.Home
+{
+    public class HomeModel
+    {
+
+        public int IdHome { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+
+        public string RodapeDescricao { get; set; }
+
+        ContatosModel Contatos { get; set; }
+    }
+}
