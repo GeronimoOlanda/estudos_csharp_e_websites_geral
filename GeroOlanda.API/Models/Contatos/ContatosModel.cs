990d69c63ddca70ce596922eab2fd57b21ecd926
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-12 13:26:42 -0300

    criando a nossa model contatos.

---
+++ GeroOlanda.API/Models/Contatos/ContatosModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GeroOlanda.API.Models.Contatos
+{
+    public class ContatosModel
+    {
+        public int idContatos { get; set; }
+        public string contatos { get; set; }
+        public string DDD { get; set; }
+    }
+}
