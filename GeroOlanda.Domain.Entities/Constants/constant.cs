1143053c8a134176766f14081bec826fc59784c2
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-14 09:43:46 -0300

    criando a constante do nome do autor.

---
+++ GeroOlanda.Domain.Entities/Constants/constant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Entities.Constants
+{
+    public class Constant
+    {
+
+        public const string NomeAutor = "Geronimo Olanda";
+    }
+}
