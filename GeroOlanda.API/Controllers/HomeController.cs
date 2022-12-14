c6694da7eaa37ee720ceeeef8732fdd408c9577a
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-14 11:57:03 -0300

    Atualizando nossa controller.

--- GeroOlanda.API/Controllers/HomeController.cs
+++ GeroOlanda.API/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using GeroOlanda.Domain.Entities;
 using GeroOlanda.Domain.Entities.Dtos;
+using GeroOlanda.Domain.Repository.Impl.GeroDbContext;
 using GeroOlanda.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
@@ -11,20 +13,31 @@ namespace GeroOlanda.API.Controllers
     public class HomeController : ControllerBase
     {
         private readonly IHomeService _homeService;
-
-        public HomeController(  IHomeService homeService)
+        private readonly ICadastrarDadosService _cadastrarDadosService;
+        public HomeController(IHomeService homeService, ICadastrarDadosService cadastrarDadosService)
         {
             _homeService = homeService;
+            _cadastrarDadosService = cadastrarDadosService;
         }
 
-       [HttpGet]
+        [HttpGet]
         [SwaggerOperation("Consulta dados da pagina Home")]
         public async Task<ActionResult<IList<HomeDTO>>> ConsultarDados(string idHome, decimal IdUsuario)
         {
-            
+          
             var response = await _homeService.ConsultarDadosPorIdUsuario(IdUsuario);
             return Ok(response);
         }
 
+        [HttpPost]
+        [SwaggerOperation("Cadastrar Descricao")]
+        [SwaggerResponse(200, "", typeof(List<Home>))]
+        public async Task<IActionResult> GravarDadosDescricao(string Descricao)
+        {
+            await _homeService.Cadastrar(Descricao);
+
+            return Ok(_homeService.Cadastrar(Descricao));
+        }
+     
     }
 }
