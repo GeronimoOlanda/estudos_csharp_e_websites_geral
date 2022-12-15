7e311bc6879e9c484c1bcd230edbf2f809fd49d0
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-15 00:22:26 -0300

    Criação contatosDto.

---
+++ GeroOlanda.Domain.Entities/Dtos/ContatosDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Entities.Dtos
+{
+    public class ContatosDto
+    {
+       public string ContatosId { get; set; }
+       public string Telefone { get; set; }
+    }
+}
