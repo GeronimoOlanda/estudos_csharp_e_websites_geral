922892ef3afebf95104f45cb0334bbab1c929c44
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-16 00:50:42 -0300

    criação da nossa util model.

---
+++ GeroOlanda.Domain.Entities/Util/Utils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace GeroOlanda.Domain.Entities.Util
+{
+    public class Utils
+    {
+        private  RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+
+        public string PegaSenha(string senha)
+        {
+            var senhaHash = GerarHashSenha(senha);
+
+            return senhaHash;
+        }
+        public string GerarHashSenha(string password)
+        {
+            byte[] salt = new byte[16];
+            rngCsp.GetBytes(salt);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt);
+
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            string hashSenha = Convert.ToBase64String(hashBytes);
+
+            return hashSenha;
+        }
+    }
+}
