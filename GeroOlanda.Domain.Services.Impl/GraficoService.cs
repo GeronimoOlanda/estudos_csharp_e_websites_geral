67a70d3b7dde5be6b4c3f84fbca835529f420044
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-19 00:07:44 -0300

    criação da service e interface do grafico.

---
+++ GeroOlanda.Domain.Services.Impl/GraficoService.cs
@@ -0,0 +1,17 @@
+﻿using GeroOlanda.Domain.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Services.Impl
+{
+    public class GraficoService : IGraficoService
+    {
+        public GraficoDTO GravarDadosParaOGrafico(GraficoDTO graficoDados)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GeroOlanda.Domain.Services/IGraficoService.cs
@@ -0,0 +1,17 @@
+﻿using GeroOlanda.Domain.Entities;
+using GeroOlanda.Domain.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Services
+{
+    public interface IGraficoService
+    {
+        GraficoDTO GravarDadosParaOGrafico(GraficoDTO graficoDados);
+
+
+    }
+}
