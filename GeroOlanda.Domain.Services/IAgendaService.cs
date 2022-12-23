706950eef0709a09b3d7e63b2d140b39260c6601
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-23 20:55:15 -0300

    Criação Interface da service agenda

---
+++ GeroOlanda.Domain.Services/IAgendaService.cs
@@ -0,0 +1,16 @@
+﻿using GeroOlanda.Domain.Entities;
+using GeroOlanda.Domain.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Services
+{
+    public interface IAgendaService
+    {
+        AgendaDTO CriarDadosAgenda(AgendaDTO agendaDTO);
+        IList<AgendaDTO> ConsultarDadosAgenda(AgendaDTO consultarAgenda);
+    }
+}
