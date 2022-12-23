eee6a4aa67f66a75c40f2eb51153f5770c8a46e5
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-23 20:55:45 -0300

    Criação da AgendaDTO

---
+++ GeroOlanda.Domain.Entities/Dtos/AgendaDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Entities.Dtos
+{
+    public class AgendaDTO
+    {
+    }
+}
