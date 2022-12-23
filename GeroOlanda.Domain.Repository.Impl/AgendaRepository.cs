e2addf6cd105d80a4808e500969c37bf9025b689
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-23 20:56:00 -0300

    Criação da AgendaRepository

---
+++ GeroOlanda.Domain.Repository.Impl/AgendaRepository.cs
@@ -0,0 +1,35 @@
+﻿using GeroOlanda.Domain.Entities;
+using GeroOlanda.Domain.Entities.Dtos;
+using GeroOlanda.Domain.Repository.Impl.GeroDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Repository.Impl
+{
+    public class AgendaRepository : IAgendaRepository
+    {
+        protected GeroOlandaDbContext _context;
+
+        public AgendaRepository(GeroOlandaDbContext context)
+        {
+            _context = context;
+        }
+        public AgendaRepository()
+        {
+
+        }
+
+        public AgendaDTO CriarDadosAgenda(AgendaDTO agendaDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<AgendaDTO> ConsultarDadosAgenda(AgendaDTO consultarAgenda)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
