2b3ec5a994c0a692b9ce51d7ef55f5426a16c998
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-23 20:56:10 -0300

    Criação da Service.

---
+++ GeroOlanda.Domain.Services.Impl/AgendaService.cs
@@ -0,0 +1,30 @@
+﻿using GeroOlanda.Domain.Entities;
+using GeroOlanda.Domain.Entities.Dtos;
+using GeroOlanda.Domain.Repository;
+using GeroOlanda.Domain.Services;
+
+namespace GeroOlanda.Domain.Services.Impl
+{
+    public class AgendaService :IAgendaService
+    {
+        private readonly IAgendaRepository _agendaRepository;
+        public AgendaService()
+        {
+           
+        }
+        public AgendaService(IAgendaRepository agendaRepository)
+        {
+            _agendaRepository = agendaRepository;
+        }
+
+        public IList<AgendaDTO> ConsultarDadosAgenda(AgendaDTO consultarAgenda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AgendaDTO CriarDadosAgenda(AgendaDTO agendaDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
