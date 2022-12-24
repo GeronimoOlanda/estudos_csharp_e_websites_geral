dd12f5abbd4ef312df92f88262b3c1fc725df060
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-23 22:23:50 -0300

    Mapeando o DTO

--- GeroOlanda.Domain.Entities/Dtos/AgendaDTO.cs
+++ GeroOlanda.Domain.Entities/Dtos/AgendaDTO.cs
@@ -8,5 +8,11 @@ namespace GeroOlanda.Domain.Entities.Dtos
 {
     public class AgendaDTO
     {
+        public int Id_Agenda { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Observacoes { get; set; }
+        public string Detalhes { get; set; }
+        public string flagExibir { get; set; }
     }
 }
