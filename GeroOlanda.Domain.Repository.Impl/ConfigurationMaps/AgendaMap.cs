943e2c43e81b97481f84092e53b253de55233412
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-23 22:24:16 -0300

    Mapeando diretamente com o banco.

---
+++ GeroOlanda.Domain.Repository.Impl/ConfigurationMaps/AgendaMap.cs
@@ -0,0 +1,42 @@
+﻿using GeroOlanda.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Repository.Impl.ConfigurationMaps
+{
+    public class AgendaMap : IEntityTypeConfiguration<Agenda>
+    {
+
+        public void Configure(EntityTypeBuilder<Agenda> builder)
+        {
+            //primery key
+            builder.HasKey(x => x.Id_Agenda);
+
+            //Atributos
+            builder.Property(e => e.Titulo)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(e => e.Descricao)
+                .HasMaxLength(255)
+                .IsRequired();
+            builder.Property(e => e.Observacoes)
+             .HasMaxLength(255)
+             .IsRequired();
+            builder.Property(e => e.Detalhes)
+             .HasMaxLength(255)
+             .IsRequired();
+            builder.Property(e => e.flagExibir)
+             .HasMaxLength(5)
+             .IsRequired();
+
+
+
+        }
+    }
+}
