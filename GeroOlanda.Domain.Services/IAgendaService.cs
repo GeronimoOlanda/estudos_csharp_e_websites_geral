c834a975a04bc711550f1abfb94acbc6f2b0d987
GeronimoOlanda <geronimo.olanda@outlook.com>
2022-12-31 12:14:02 -0300

    criação interface

--- GeroOlanda.Domain.Services/IAgendaService.cs
+++ GeroOlanda.Domain.Services/IAgendaService.cs
@@ -12,7 +12,7 @@ namespace GeroOlanda.Domain.Services
     {
         AgendaDTO CriarDadosAgenda(AgendaDTO agendaDTO);
         IList<AgendaDTO> ConsultarDadosAgenda(int idIntervencao, string Descricao);
-        IList<AgendaDTO> RetornoDadosFlag(string flag);
+        IList<AgendaDTO> RetornoDados(int idUsuario);
 
         string RemoveDaBase(int idUsuario);
     }
