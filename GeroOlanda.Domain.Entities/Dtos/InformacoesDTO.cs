71e04b40332ad2f60c3a7616460daff08a6e9435
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-01-09 22:21:28 -0300

    Criação da dto de informações, para preencher com todos os dados possiveis.

---
+++ GeroOlanda.Domain.Entities/Dtos/InformacoesDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Entities.Dtos
+{
+    public class InformacoesDTO
+    {
+        public int Id_Agenda { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Observacoes { get; set; }
+        public string Detalhes { get; set; }
+        public string flagExibir { get; set; }
+    }
+}
