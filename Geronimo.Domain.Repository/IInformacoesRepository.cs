b422915222066023d9552c61b4e1b28634639861
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-01-09 22:19:54 -0300

    Criação da interface da repository informações

---
+++ Geronimo.Domain.Repository/IInformacoesRepository.cs
@@ -0,0 +1,17 @@
+﻿using GeroOlanda.Domain.Entities;
+using GeroOlanda.Domain.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Repository
+{
+    public interface IInformacoesRepository
+    {
+
+        InformacoesDTO CadastrarInformacoes(InformacoesDTO informacoesDTO);
+
+    }
+}
