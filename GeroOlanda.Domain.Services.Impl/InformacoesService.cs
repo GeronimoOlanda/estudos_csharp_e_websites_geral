d4bffe2dd04bf2c98a002daa38f8883599efada6
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-01-09 22:24:10 -0300

    Criação da service de informações.

---
+++ GeroOlanda.Domain.Services.Impl/InformacoesService.cs
@@ -0,0 +1,32 @@
+﻿using GeroOlanda.Domain.Entities;
+using GeroOlanda.Domain.Entities.Dtos;
+using GeroOlanda.Domain.Repository;
+using GeroOlanda.Domain.Services;
+
+namespace GeroOlanda.Domain.Services.Impl
+{
+    public class InformacoesService :IInformacoesService
+    {
+        private readonly IAgendaRepository _agendaRepository;
+        public InformacoesService()
+        {
+           
+        }
+        public InformacoesService(IAgendaRepository agendaRepository)
+        {
+            _agendaRepository = agendaRepository;
+        }
+
+        public InformacoesDTO CadastrarInformacoes(InformacoesDTO informacoesDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AgendaDTO CriarDadosAgenda(AgendaDTO agendaDTO)
+        {
+            var response = _agendaRepository.CriarDadosAgenda(agendaDTO);
+
+            return response;
+        }
+    }
+}
