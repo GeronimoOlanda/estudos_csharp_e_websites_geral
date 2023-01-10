af4df808c0f67f09743b22bc4ae920ff96e3db72
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-01-09 22:20:10 -0300

    Criação da controller informações

---
+++ GeroOlanda.API/Controllers/InformacoesController.cs
@@ -0,0 +1,34 @@
+
+using GeroOlanda.Domain.Entities;
+using GeroOlanda.Domain.Entities.Dtos;
+using GeroOlanda.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.WebEncoders.Testing;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace GeroOlanda.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InformacoesController : ControllerBase
+    {
+        private readonly IInformacoesService _informacoesService;
+
+        public InformacoesController(IInformacoesService informacoesService)
+        {
+            _informacoesService = informacoesService;
+        }
+
+        [HttpPost]
+        [SwaggerOperation("Cadastrar Informações")]
+        [SwaggerResponse(200, "", typeof(List<AgendaDTO>))]
+        public InformacoesDTO CadastrarInformacoes([FromBody] InformacoesDTO agendaDTO)
+        {
+            var response = _informacoesService.CadastrarInformacoes(agendaDTO);
+            return response;
+        }
+
+       
+    }
+}
