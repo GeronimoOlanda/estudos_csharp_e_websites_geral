d5c56604f19a1ca5af6e9d5ec27cd4644bcb19a6
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-01-09 22:22:30 -0300

    Criação da nossa repository, aonde usaremos linq para consultar na base de dados sql server.

---
+++ GeroOlanda.Domain.Repository.Impl/InformacoesRepository.cs
@@ -0,0 +1,29 @@
+﻿using GeroOlanda.Domain.Entities;
+using GeroOlanda.Domain.Entities.Dtos;
+using GeroOlanda.Domain.Repository.Impl.GeroDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Repository.Impl
+{
+    public class InformacoesRepository : IInformacoesRepository
+    {
+        protected GeroOlandaDbContext _context;
+
+        public InformacoesRepository(GeroOlandaDbContext context)
+        {
+            _context = context;
+        }
+        public InformacoesRepository()
+        {
+
+        }
+        public InformacoesDTO CadastrarInformacoes(InformacoesDTO informacoesDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
