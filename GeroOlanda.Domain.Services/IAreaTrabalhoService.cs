bebc377430199b21e0c80fe00b8183736c59a96a
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-05-03 01:45:30 -0300

    Criação da Interface

---
+++ GeroOlanda.Domain.Services/IAreaTrabalhoService.cs
@@ -0,0 +1,15 @@
+﻿using GeroOlanda.Domain.Entities;
+using GeroOlanda.Domain.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeroOlanda.Domain.Services
+{
+    public interface IAreaTrabalhoService
+    {
+        IList<AreaTrabalhoDTO> AreaTrabalhoDetalhes(IList<AreaTrabalhoDTO> AreaTrabalho);
+    }
+}
