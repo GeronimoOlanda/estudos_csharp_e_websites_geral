a0d3cb67f8654fa1b896c2a98425aaf848917969
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-05-03 01:44:46 -0300

    criação Area de Trabalho Controller

---
+++ GeroOlanda.API/Controllers/AreaTrabalho/AreaTrabalhoController.cs
@@ -0,0 +1,31 @@
+using GeroOlanda.Domain.Entities;
+using GeroOlanda.Domain.Entities.Dtos;
+using GeroOlanda.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.WebEncoders.Testing;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace GeroOlanda.API.Controllers.AreaTrabalho
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AreaTrabalhoController : ControllerBase
+    {
+        private readonly IAreaTrabalhoService _areaTrabalhoService;
+
+        public AreaTrabalhoController(IAreaTrabalhoService areaTrabalhoService)
+        {
+            _areaTrabalhoService = areaTrabalhoService;
+        }
+
+        [HttpPost]
+        [SwaggerOperation("Cadastrar Detalhes Area de Trabalho")]
+        [SwaggerResponse(200, "", typeof(List<AreaTrabalhoDTO>))]
+        public IList<AreaTrabalhoDTO> AreaTrabalhoDetalhes([FromBody] IList<AreaTrabalhoDTO> AreaTrabalho)
+        {
+            var response = _areaTrabalhoService.AreaTrabalhoDetalhes(AreaTrabalho);
+            return response;
+        }
+    }
+}
